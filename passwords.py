2cc885af6949f955641005df05e67573a79da8c0
yahyasaadi <yahyasaadi9219@gmail.com>
2022-01-21 17:28:53 +0300

    added a method to generate a random password

--- passwords.py
+++ passwords.py
@@ -1,4 +1,5 @@
 # Passwords class
+import random
 class Password:
 
     def __init__(self, siteName, user_name, pass_word):
@@ -33,9 +34,23 @@ class Password:
         return Password.passwords.remove(self)
 
 
+    
+    def generate_pw():
+        '''
+        Added a method to generate a random password for the user
+        '''
+        random_pass = "abcdefghijklmnopqrstuvwxyz0123456789"
+        return ''.join((random.choice(random_pass) for i in range(8)))
+
+
+
+
+
+
 # TESTING PART
-# pw = Password("IG", "yahyasaadi", "1234")
+pw = Password("IG", "yahyasaadi", "1234")
 # pw.save_password()
 # pw2 = Password("TW", "yahya", "1234")
 # pw2.save_password()
 # print(len(Password.passwords))
+print(Password.generate_pw())

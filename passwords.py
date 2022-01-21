78a3cbaa36c657f9d2f3f398a7699343e3e02d78
yahyasaadi <yahyasaadi9219@gmail.com>
2022-01-21 16:52:49 +0300

    added the init method to for instances

---
+++ passwords.py
@@ -0,0 +1,11 @@
+# Passwords class
+class Password:
+
+    def __init__(self, siteName, user_name, pass_word):
+        """
+        Class that generates new instances of accounts
+        """
+        
+        self.siteName = siteName
+        self.user_name = user_name
+        self.pass_word = pass_word

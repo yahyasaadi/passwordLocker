5059b8f6c6d177e01cede53f306c06fff3606ac9
yahyasaadi <yahyasaadi9219@gmail.com>
2022-01-21 16:58:23 +0300

    added a methods to add or delete a password

--- passwords.py
+++ passwords.py
@@ -5,7 +5,21 @@ class Password:
         """
         Class that generates new instances of accounts
         """
-        
+
         self.siteName = siteName
         self.user_name = user_name
         self.pass_word = pass_word
+
+    passwords = []
+    def save_password(self):
+        '''
+        Method to save a password to a list
+        '''
+        return Password.passwords.append(self)
+
+    
+    def delete_password(self):
+        '''
+        Method to delete a password from the list
+        '''
+        return Password.passwords.remove(self)

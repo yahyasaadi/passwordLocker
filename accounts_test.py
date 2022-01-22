0b71f14a18cd5197782e764995d7741accda343a
yahyasaadi <yahyasaadi9219@gmail.com>
2022-01-22 10:08:26 +0300

    added an init test methods to test for proper initialization

--- accounts_test.py
+++ accounts_test.py
@@ -8,3 +8,13 @@ class TestAccount(unittest.TestCase):
         Set up method to run before each test cases
         """
         self.new_account = Account("yahyanoor", "1234")
+
+
+    # The first test
+    def test_init(self):
+        """
+        test_init test case to test if the object is initialized propely
+        """
+
+        self.assertEqual(self.new_account.userName, "yahyanoor")
+        self.assertEqual(self.new_account.password, "1234")

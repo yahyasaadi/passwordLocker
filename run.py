d0af82ba0beadbb6e7f806c4617d7af328ded2a1
yahyasaadi <yahyasaadi9219@gmail.com>
2022-01-23 11:04:19 +0300

    added functions to create, save and remove accounts

--- run.py
+++ run.py
@@ -1,3 +1,19 @@
 from accounts import Account
 from passwords import Password
 
+# fucntion to create new method
+def create_account(usrname, password):
+    account = Account(usrname, password)
+    return account
+
+
+# fucntion to save new accounts
+def save_account(account):
+    account.save_account()
+
+# function to delete an account
+def del_account(account):
+    account.delete_account()
+
+
+

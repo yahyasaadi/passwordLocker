94c41e111656b826858c3e8ff8103ae27bb80aef
yahyasaadi <yahyasaadi9219@gmail.com>
2022-01-24 10:06:52 +0300

    added the setUp method and the init method

--- passwords_test.py
+++ passwords_test.py
@@ -1,2 +1,22 @@
 import unittest
 from passwords import Password
+
+
+class TestAccount(unittest.TestCase):
+    
+    def setUp(self):
+        """
+        Set up method to run before each test cases
+        """
+        self.new_password = Password("Instagram", "yahyanoor", "1234")
+
+    
+    # The first test
+    def test_init(self):
+        """
+        test_init test case to test if the object is initialized propely
+        """
+
+        self.assertEqual(self.new_password.siteName, "Instagram")
+        self.assertEqual(self.new_password.user_name, "yahyanoor")
+        self.assertEqual(self.new_password.pass_word, "1234")

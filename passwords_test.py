f9892564160e290b0587ed819cb94357eaeb502e
yahyasaadi <yahyasaadi9219@gmail.com>
2022-01-24 10:11:31 +0300

    added the tearDown method

--- passwords_test.py
+++ passwords_test.py
@@ -20,3 +20,19 @@ class TestAccount(unittest.TestCase):
         self.assertEqual(self.new_password.siteName, "Instagram")
         self.assertEqual(self.new_password.user_name, "yahyanoor")
         self.assertEqual(self.new_password.pass_word, "1234")
+
+
+    # The Second Test
+    def test_save_account(self):
+        """
+        test_save_account is a test case to see if an account object is saved into the account list
+        """
+        self.new_password.save_password()
+        self.assertEqual(len(Password.passwords), 1)
+
+    
+    def tearDown(self):
+        """
+        This method clear up after a test case is done
+        """
+        Password.passwords = []
